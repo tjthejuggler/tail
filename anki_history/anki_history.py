41c9cc39e9c77aefda7c7a703c37c71434fa4b9d
tjthejuggler <tjthejuggler@gmail.com>
2023-04-04 20:51:23 +0200

    started working on character creation from personal data

--- anki_history/anki_history.py
+++ anki_history/anki_history.py
@@ -1,7 +1,7 @@
 import requests
 import json
-import datetime
 import time
+import re
 
 def invoke(action, **params):
     request_data = {'action': action, 'params': params, 'version': 6}
@@ -25,18 +25,31 @@ def get_note_id_from_card_id(card_id):
         print("Card not found.")
         return None
 
+def remove_images_and_hyperlinks(text):
+    img_pattern = r'<br><br><img src="img\d+\.jpg">'
+    hyperlink_pattern = r'<a href="https?://[^"]+">[^<]+</a>'
+    text = re.sub(img_pattern, '', text)
+    text = re.sub(hyperlink_pattern, '', text)
+    return text
+
 def get_reviewed_cards(deck_name, target_date):
     note_ids = invoke('findNotes', query=f'deck:{deck_name}')
     start_time, end_time = unix_time_range(target_date)
     reviewed_notes = []
     note_ids = [num for num in note_ids if start_time <= num // 1000 <= end_time]
+    #print(note_ids)
     for note_id in note_ids:
         note_timestamp = note_id // 1000
         if note_timestamp >= start_time and note_timestamp <= end_time:
             note_info = invoke("notesInfo", notes=[note_id])
-            front = note_info[0]["fields"]["Front"]["value"]
-            back = note_info[0]["fields"]["Back"]["value"]
-            reviewed_notes.append((front, back))    
+            #print(note_info[0]["fields"])
+            note_text = None
+            if "Front" in note_info[0]["fields"]:
+                note_text = remove_images_and_hyperlinks(note_info[0]["fields"]["Front"]["value"] + " " + note_info[0]["fields"]["Back"]["value"])
+            elif "Text" in note_info[0]["fields"]:
+                note_text = note_info[0]["fields"]["Text"]["value"].replace("{{c1::", "").replace("}}", "")
+            if note_text:
+                reviewed_notes.append(note_text.replace('<br>',''))    
     return reviewed_notes
 
 def unix_time_range(date):
@@ -44,10 +57,5 @@ def unix_time_range(date):
     end_of_day = start_of_day + 86399 # 86399 seconds = 23 hours, 59 minutes, and 59 seconds
     return start_of_day, end_of_day
 
-if __name__ == '__main__':
-    deck_name = '...MyDiscoveries'
-    target_date = '2023-03-29'
-    print(target_date)
-    reviewed_cards = get_reviewed_cards(deck_name, target_date)
-    print(reviewed_cards)
+
 
--- storytell/character_creator_personal_data.py
+++ storytell/character_creator_personal_data.py
@@ -1,8 +1,64 @@
-#habits
-#clipboard
-#personal records /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/personal_records.txt
-#completed todos /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/tododb.txt 
+import sys
+from pathlib import Path
+import json
 
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'anki_history'))
+
+import anki_history
+
+target_date = '2023-03-19'
+
+#personal records /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/personal_records.txt
+def find_records_on_date(date):
+    with open('/home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/personal_records.txt', 'r') as f:
+        data = json.load(f)
+    result = []
+    for name, content in data.items():
+        if date in content['records']:
+            result.append({
+                'name': name,
+                'description': content['description'],
+                'record': content['records'][date]
+            })
+    return result
+
+#completed todos /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/tododb.txt 
+def get_date_from_simple_dict(file, date):
+    result = []
+    with open(file, 'r') as f:
+        data = json.load(f)
+    for key in data.keys():
+        if date in key:
+            result.append(data[key])
+    return result
+
+#anki cards made
+deck_name = '...MyDiscoveries'
+print("\nANKI\n" + "\n".join(anki_history.get_reviewed_cards(deck_name, target_date)) + "\n")
+
+records = find_records_on_date(target_date)
+print("PERSONAL RECORDS")
+for record in records:
+    print(f"{record['name']}: {record['description']} - Record: {record['record']}")
+
+todo_items = get_date_from_simple_dict('/home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/tododb.txt', target_date)
+print("TODO ITEMS\n" + "\n".join(todo_items) + "\n")
+
+unusual_experiences = get_date_from_simple_dict('/home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/unusual_experiences.txt', target_date)
+print("UNUSUAL EXPERIENCES\n" + "\n".join(unusual_experiences) + "\n")
+
+podcast_descriptions = get_date_from_simple_dict('/home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/podcasts_history.txt', target_date)
+print("PODCAST DESCRIPTIONS\n" + "\n".join(podcast_descriptions) + "\n")
+
+clipboard_items = get_date_from_simple_dict('/home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/clipboard/clipboarddb.txt', target_date)
+print("CLIPBOARD ITEMS\n" + "\n".join(clipboard_items) + "\n")
+
+
+#habits /home/lunkwill/Documents/obsidian_note_vault/noteVault/habitsdb.txt
 #location
-#unusual experiences /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/unusual_experiences.txt
-#podcasts /home/lunkwill/Documents/obsidian_note_vault/noteVault/tail/podcasts_history.txt
+
+
+#todo
+#maybe I want to not take clipboard items from inside VSCode, or maybe just take some of them. Maybe i could just get rid of anything that has too high of a character:special character ratio?
+
+#i should probably remove anything that is a duplicate?
